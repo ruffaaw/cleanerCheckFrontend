d0cfc9131408697d5d394216b96ff8fcc0f0535e
ruffaaw <rafal.p.ciupek@gmail.com>
2025-11-13 18:59:20 +0100

    feat: corrected sidebar and added whoAmI request

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import * as React from "react";
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-  Toilet,
-  User,
-} from "lucide-react";
-
+import { Toilet, User } from "lucide-react";
 import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
 import {
@@ -23,36 +9,63 @@ import {
   SidebarFooter,
   SidebarRail,
 } from "@/components/ui/sidebar";
-
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-  },
-  panel: [
-    {
-      title: "Pracownicy",
-      url: "/pracownicy",
-      icon: User,
-    },
-    {
-      title: "Pomieszczenia",
-      url: "/pomieszczenia",
-      icon: Toilet,
-    },
-  ],
-};
+import React, { useEffect, useState } from "react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [user, setUser] = useState<{ name: string } | null>(null);
+
+  const fetchUser = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/whoami`,
+        {
+          method: "GET",
+          credentials: "include",
+          cache: "no-store",
+        }
+      );
+
+      if (!res.ok) throw new Error("Not authenticated");
+
+      const data = await res.json();
+      const newUser = { name: data.data.name };
+      setUser(newUser);
+
+      localStorage.setItem("userData", JSON.stringify(newUser));
+    } catch (err) {
+      console.error("Nie udało się pobrać użytkownika:", err);
+      setUser(null);
+      localStorage.removeItem("userData");
+    }
+  };
+
+  useEffect(() => {
+    const saved = localStorage.getItem("userData");
+    if (saved) {
+      try {
+        const parsed = JSON.parse(saved);
+        setUser(parsed);
+      } catch {
+        localStorage.removeItem("userData");
+      }
+    } else {
+      fetchUser();
+    }
+  }, []);
+
+  const data = {
+    panel: [
+      { title: "Pracownicy", url: "/pracownicy", icon: User },
+      { title: "Pomieszczenia", url: "/pomieszczenia", icon: Toilet },
+    ],
+  };
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarContent>
         <NavMain items={data.panel} />
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
+      <SidebarFooter>{user ? <NavUser user={user} /> : ""}</SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
